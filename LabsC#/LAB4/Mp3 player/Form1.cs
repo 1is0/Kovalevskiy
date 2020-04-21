9393d0a48bd6ebe5231d04d4e39c239c2cf23c2e
MishaKoval <62940078+MishaKoval@users.noreply.github.com>
2020-04-21 21:31:18 +0300

    Add files via upload

---
+++ LabsC#/LAB4/Mp3 player/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private MP3 mp3Player;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Filter = "Mp3 File|*.mp3";
+                dlg.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyComputer);
+
+                if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    mp3Player = new MP3(dlg.FileName);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (mp3Player != null)
+            {
+                // mp3Player.Stop();
+                mp3Player.Play();
+            }
+            else MessageBox.Show("Файл не выбран!");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (mp3Player != null)
+                mp3Player.Stop();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (mp3Player != null)
+                mp3Player.SetVolume(trackBar1.Value);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (mp3Player != null) mp3Player.Stop();
+            string path = listBox1.SelectedItem.ToString();
+            mp3Player = new MP3(path);
+            if (mp3Player!=null) mp3Player.Play();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string path = null;
+            using (var dialog = new FolderBrowserDialog())
+                if (dialog.ShowDialog() == DialogResult.OK)
+                    path = dialog.SelectedPath;
+            string[] files = null;
+            try { files = Directory.GetFiles(path, "*.mp3", SearchOption.AllDirectories); }
+            catch { }
+            if (files != null)
+            {
+                foreach (string file in files)
+                {
+                    listBox1.Items.Add(file);
+                }
+            }
+        }
+    }
+}
---
+++ LabsC#/LAB4/Mp3 player/MP3.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class MP3 : IDisposable
+    {
+        [DllImport("winmm.dll")]
+        static extern Int32 mciSendString(string command,
+           StringBuilder buffer, int bufferSize, IntPtr hwndCallback);
+        public MP3(string filename)
+        {
+            const string FORMAT = @"open ""{0}"" type mpegvideo alias MediaFile";
+            string command = String.Format(FORMAT, filename);
+            mciSendString(command, null, 0, IntPtr.Zero);
+        }
+
+
+        public void Play()
+        {
+            string command = "play MediaFile";
+            mciSendString(command, null, 0, IntPtr.Zero);
+        }
+
+        public void Stop()
+        {
+            string command = "close MediaFile";
+            mciSendString(command, null, 0, IntPtr.Zero);
+
+        }
+
+
+        public void SetVolume(int volume)
+        {
+            string command = "setaudio MediaFile volume to " + (volume * 100).ToString();
+            mciSendString(command, null, 0, IntPtr.Zero);
+        }
+
+        public void Dispose()
+        {
+            Stop();
+        }
+    }
+}
