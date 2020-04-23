ce34a6ebe72d0d54488c0fa70678a152fa1b29b2
MishaKoval <62940078+MishaKoval@users.noreply.github.com>
2020-04-23 17:06:35 +0300

    Add files via upload

---
+++ LabsC#/Lab6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Moving(IMovable movable)
+        {
+            movable.Move();
+        }
+        struct nonMove : IMovable
+        {
+            public void Move() { Console.WriteLine("GG"); }
+        }
+
+        static void Main()
+        {
+            Jugyli j1 = new Jugyli(Avto.Color.Белый, 500, 400, DateTime.Now);
+            Jugyli j2 = new Jugyli(Avto.Color.Желтый, 400, 4000, DateTime.Now);
+            Jugyli j3 = new Jugyli(Avto.Color.Розовый, 300, 300, DateTime.Now);
+            Audi a1 = new Audi(Avto.Color.Красный, 300, 400, DateTime.Now);
+
+
+            Jugyli[] jugylis = new Jugyli[] { j1, j2, j3 };
+            for (int i = 0; i < jugylis.Length; i++)
+            {
+                Console.WriteLine(jugylis[i].ToString());
+            }
+            Console.WriteLine("Сортировка по цене.....\n");
+            Array.Sort(jugylis);
+            for (int i = 0; i < jugylis.Length; i++)
+            {
+                Console.WriteLine(jugylis[i].ToString());
+            }
+            Moving(a1);
+            for (int i = 0; i < jugylis.Length; i++)
+            {
+                Moving(jugylis[i]);
+            }
+            nonMove nonMove = new nonMove();
+            Moving(nonMove);
+        }
+    }
+}
---
+++ LabsC#/Lab6/Transport.cs
@@ -0,0 +1,109 @@
+﻿using System;
+namespace Lab6
+{
+    class Transport : IComparable,IMovable
+    {
+
+        public virtual int CompareTo(object obj)
+        {
+            Transport p = obj as Transport;
+            if (p != null) return this.speed.CompareTo(p.speed);
+            else
+                throw new NotImplementedException();
+        }
+
+        public enum TypeOfMedium : byte
+        {
+            Ground,
+            Air,
+            Water,
+            Space
+        }
+        protected virtual TypeOfMedium TypeOfM { get; set; }
+        public enum ByAppointment : byte
+        {
+            Cargo,
+            Passenger,
+            Any
+        }
+        protected virtual ByAppointment ByApp { get; set; }
+        protected virtual int speed { get; set; }
+        protected virtual int procentoffuel { get; set; }
+        protected virtual int capasity { get; set; }
+        protected virtual bool working { get; set; } = true;
+
+        public static int counter = 0;
+
+        public Transport()
+        {
+            TypeOfM = TypeOfMedium.Ground;
+            ByApp = ByAppointment.Passenger;
+            speed = 0;
+            working = true;
+            procentoffuel = 100;
+            capasity = 4;
+            counter++;
+        }
+
+        public Transport(int speed, int fuel, int capasity, Transport.TypeOfMedium TypeOfM, Transport.ByAppointment ByApp, bool working)
+        {
+            this.speed = speed;
+            procentoffuel = fuel;
+            this.capasity = capasity;
+            this.TypeOfM = TypeOfM;
+            this.ByApp = ByApp;
+            this.working = working;
+            counter++;
+        }
+
+        public override string ToString()
+        {
+            string returning = $"Транспорт:";
+            switch (TypeOfM)
+            {
+                case TypeOfMedium.Ground: returning += "наземный"; break;
+                case TypeOfMedium.Air: returning += "воздушный"; break;
+                case TypeOfMedium.Space: returning += "космический"; break;
+                case TypeOfMedium.Water: returning += "водный"; break;
+
+            }
+            switch (ByApp)
+            {
+                case ByAppointment.Passenger: returning += ",пассажирский"; break;
+                case ByAppointment.Cargo: returning += ",грузовой"; break;
+                case ByAppointment.Any: returning += ",любой"; break;
+            }
+            if (working) { returning += $"\nРаботает"; } else returning += $"\nНе работает";
+            returning += $"\nВместимость: {capasity} ";
+            returning += $"\nСкорость:{speed}";
+            returning += $"\nПроцент топлива:{procentoffuel}";
+            return returning;
+        }
+
+        public virtual void Move()
+        {
+            if (working)
+            {
+                while (procentoffuel > 0)
+                {
+                    procentoffuel -= (speed / 5);
+                    Console.Beep(576, 576);
+                    if (TypeOfM == TypeOfMedium.Ground) Console.WriteLine("Едем");
+                    if (TypeOfM == TypeOfMedium.Air) Console.WriteLine("Летим");
+                    if (TypeOfM == TypeOfMedium.Water) Console.WriteLine("Плывем");
+                    if (TypeOfM == TypeOfMedium.Space) Console.WriteLine("Земля в иллюминаторе");
+                }
+                Console.WriteLine("Топливо закончилось!");
+            }
+            else Console.WriteLine("Сломан!");
+        }
+        public virtual void Refueling()
+        {
+            if (procentoffuel < 100)
+            {
+                Console.WriteLine("Заправка...\n");
+                procentoffuel = 100;
+            }
+        }
+    }
+}
