40120ef1cabc58158898c7dab44bbe2ace69f7dd
MishaKoval <62940078+MishaKoval@users.noreply.github.com>
2020-05-30 13:04:05 +0300

    Add files via upload

---
+++ LabsC#/Lab7/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace Lab7
+{
+    class Program
+    {
+        public static RealNumber GetFractionFromString()
+        {
+            Console.Write("Fraction: ");
+            string str = Console.ReadLine();
+            while (!CheckFraction(str))
+            {
+                Console.Write("Error, repeat: ");
+                str = Console.ReadLine();
+            }
+
+            try
+            {
+                string[] arr = str.Split('/');
+                return new RealNumber(int.Parse(arr[0]), int.Parse(arr[1]));
+            }
+            catch
+            {
+                return new RealNumber(int.Parse(str), 1);
+            }
+        }
+
+        public static RealNumber Enter()
+        {
+            int a;
+            Console.WriteLine("Input 0 to write nomber as fraction or 1 for default number: ");
+            while (!int.TryParse(Console.ReadLine(), out a) || a < 0 || a > 1)
+            {
+                Console.Write("Error, repeat: ");
+            }
+            if (a == 0) return GetFractionFromString();
+            else return GetNumber();
+        }
+
+        public static int CheckNaturalNomber()
+        {
+            int a;
+            while (!int.TryParse(Console.ReadLine(), out a) || a <= 0)
+                Console.Write("Error, repeat: ");
+            return a;
+        }
+
+        public static int CheckIntNomber()
+        {
+            int a;
+            while (!int.TryParse(Console.ReadLine(), out a))
+                Console.Write("Error, repeat: ");
+            return a;
+        }
+
+        public static double CheckDouble()
+        {
+            double a;
+            while (!double.TryParse(Console.ReadLine(), out a))
+                Console.Write("Error, repeat: ");
+            return a;
+        }
+
+        public static bool CheckFraction(string fraction)
+        {
+            for (int i = 0; i < fraction.Length; i++)
+            {
+                if (i == 0 && fraction[i] == '-') i++;
+                if (fraction[i] == '/' && i != fraction.Length - 1) i++;
+                if (!char.IsDigit(fraction[i]) || (fraction[i] == '0' && i == fraction.Length - 1 && fraction[i - 1] == '/')) return false;
+            }
+            return true;
+        }
+
+        public static RealNumber GetNumber()
+        {
+            Console.Write("Number: ");
+            double number = CheckDouble();
+            return new RealNumber(number);
+        }
+
+
+
+
+
+        static void Main()
+        {
+            Console.WriteLine("Input first number");
+            var numberone = Enter();
+            Console.WriteLine("Input second number");
+            var numbertoo = Enter();
+            Console.WriteLine("First number:\n" + numberone);
+            Console.WriteLine("Second number:\n" + numbertoo);
+            int i = 1;
+            Console.WriteLine($"{i++}. {numberone.ShowFraction} > {numbertoo.ShowFraction} = " + (numberone > numbertoo));
+            Console.WriteLine($"   {numberone.ShowDefNumber} > {numbertoo.ShowDefNumber} = " + (numberone > numbertoo) + "\n");
+            Console.WriteLine($"{i++}. {numberone.ShowFraction} < {numbertoo.ShowFraction} = " + (numberone < numbertoo));
+            Console.WriteLine($"   {numberone.ShowDefNumber} < {numbertoo.ShowDefNumber} = " + (numberone < numbertoo) + "\n");
+            Console.WriteLine($"{i++}. {numberone.ShowFraction} == {numbertoo.ShowFraction} = " + (numberone == numbertoo));
+            Console.WriteLine($"   {numberone.ShowDefNumber} == {numbertoo.ShowDefNumber} = " + (numberone == numbertoo) + "\n");
+            Console.WriteLine($"{i++}. {numberone.ShowFraction} != {numbertoo.ShowFraction} = " + (numberone != numbertoo));
+            Console.WriteLine($"   {numberone.ShowDefNumber} != {numbertoo.ShowDefNumber} = " + (numberone != numbertoo) + "\n");
+            Console.WriteLine($"{i++}. {numberone.ShowFraction} + {numbertoo.ShowFraction} = " + (numberone + numbertoo).ShowFraction);
+            Console.WriteLine($"   {numberone.ShowDefNumber} + {numbertoo.ShowDefNumber} = " + (numberone + numbertoo).ShowDefNumber + "\n");
+            Console.WriteLine($"{i++}. {numberone.ShowFraction} - {numbertoo.ShowFraction} = " + (numberone - numbertoo).ShowFraction);
+            Console.WriteLine($"   {numberone.ShowDefNumber} - {numbertoo.ShowDefNumber} = " + (numberone - numbertoo).ShowDefNumber + "\n");
+            Console.WriteLine($"{i++}. {numberone.ShowFraction} * {numbertoo.ShowFraction} = " + (numberone * numbertoo).ShowFraction);
+            Console.WriteLine($"   {numberone.ShowDefNumber} * {numbertoo.ShowDefNumber} = " + (numberone * numbertoo).ShowDefNumber + "\n");
+            if (numbertoo != 0)
+            {
+                Console.WriteLine($"{i}. {numberone.ShowFraction} / {numbertoo.ShowFraction} = " + (numberone / numbertoo).ShowFraction);
+                Console.WriteLine($"   {numberone.ShowDefNumber} / {numbertoo.ShowDefNumber} = " + (numberone / numbertoo).ShowDefNumber + "\n");
+            }
+        }
+    }
+}
---
+++ LabsC#/Lab7/RealNumber.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace Lab7
+{
+    class RealNumber : IEquatable<RealNumber>, IComparable
+    {
+        public int IntegerNomber { get; set; }
+        public int NaturalNomber { get; set; }
+        double number;
+
+        public RealNumber()
+        {
+            IntegerNomber = 1;
+            NaturalNomber = 1;
+        }
+
+        public RealNumber(int i, int n)
+        {
+            IntegerNomber = i;
+            NaturalNomber = n;
+            number = (double)IntegerNomber / NaturalNomber;
+        }
+
+        public RealNumber(double num)
+        {
+            number = num;
+            int count = -1;
+            if (num % 10 == 0)
+            {
+                IntegerNomber = (int)num;
+                NaturalNomber = 1;
+                number = num;
+            }
+            else
+            {
+                for (int i = 0; (float)num % 10 != 0 || Math.Abs(num) < 1; i++)
+                {
+                    num *= 10;
+                    count++;
+                }
+                num /= 10;
+                IntegerNomber = (int)num;
+                NaturalNomber = (int)Math.Pow(10, count);
+            }
+        }
+
+        public string ShowDefNumber => $"{number}";
+
+        public string ShowFraction => $"{IntegerNomber}/{NaturalNomber}";
+
+        public override string ToString()
+        {
+            return "Number: " + ShowDefNumber + "\nFraction: " + ShowFraction + "\n";
+        }
+
+        public static bool operator >(RealNumber num1, RealNumber num2) => num1.number > num2.number;
+
+        public static bool operator <(RealNumber num1, RealNumber num2) => num1.number < num2.number;
+
+        public static bool operator ==(RealNumber num1, RealNumber num2) => num1.Equals(num2);
+
+        public static bool operator !=(RealNumber num1, RealNumber num2) => !num1.Equals(num2);
+
+        public static RealNumber operator +(RealNumber num1, RealNumber num2)
+        {
+            RealNumber num = new RealNumber();
+            num.IntegerNomber = num1.IntegerNomber * num2.NaturalNomber + num2.IntegerNomber * num1.NaturalNomber;
+            num.NaturalNomber = num1.NaturalNomber * num2.NaturalNomber;
+            num.number = num1.number + num2.number;
+            return num;
+        }
+
+        public static RealNumber operator -(RealNumber num1, RealNumber num2)
+        {
+            RealNumber num = new RealNumber();
+            num.IntegerNomber = num1.IntegerNomber * num2.NaturalNomber - num2.IntegerNomber * num1.NaturalNomber;
+            num.NaturalNomber = num1.NaturalNomber * num2.NaturalNomber;
+            num.number = num1.number - num2.number;
+            return num;
+        }
+
+        public static RealNumber operator *(RealNumber num1, RealNumber num2)
+        {
+            RealNumber num = new RealNumber();
+            num.IntegerNomber = num1.IntegerNomber * num2.IntegerNomber;
+            num.NaturalNomber = num1.NaturalNomber * num2.NaturalNomber;
+            num.number = num1.number * num2.number;
+            return num;
+        }
+
+        public static RealNumber operator /(RealNumber num1, RealNumber num2)
+        {
+            RealNumber num = new RealNumber();
+            num.IntegerNomber = num1.IntegerNomber * num2.NaturalNomber;
+            num.NaturalNomber = num1.NaturalNomber * num2.IntegerNomber;
+            if (num1 < 0 || num2 < 0)
+            {
+                num.IntegerNomber = -num.IntegerNomber;
+                num.NaturalNomber = Math.Abs(num.NaturalNomber);
+            }
+            num.number = num1.number / num2.number;
+            return num;
+        }
+
+        public override bool Equals(object obj) => ((RealNumber)obj).number == this.number;
+
+        public override int GetHashCode() => this.ToString().GetHashCode();
+
+        public static implicit operator int(RealNumber num) => (int)num.number;
+
+        public static implicit operator double(RealNumber num) => num.number;
+
+        bool IEquatable<RealNumber>.Equals(RealNumber num) => this.number == num.number;
+
+        int IComparable.CompareTo(object obj)
+        {
+            if (this.number > ((RealNumber)obj).number) return 1;
+            if (this.number < ((RealNumber)obj).number) return -1;
+            else return 0;
+        }
+    }
+}
